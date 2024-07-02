7939b282e248c366ffc4a21ece70684fabae7026
Jeff Becker <jeff@lokinet.io>
2024-07-02 17:16:19 -0400

    revert change: exclude edge router in path build hop selection. this didn't do what i thought it did and instead introduced and uneeded bias into selection. my b.

--- llarp/path/pathbuilder.cpp
+++ llarp/path/pathbuilder.cpp
@@ -266,9 +266,6 @@ namespace llarp
     Builder::GetHopsForBuild()
     {
       auto filter = [r = m_router](const auto& rc) -> bool {
-        // don't build paths to our edge nodes as a client.
-        if (not r->IsServiceNode() and r->linkManager().HasSessionTo(rc.pubkey))
-          return false;
         return not r->routerProfiling().IsBadForPath(rc.pubkey);
       };
       if (const auto maybe = m_router->nodedb()->GetRandom(filter))
